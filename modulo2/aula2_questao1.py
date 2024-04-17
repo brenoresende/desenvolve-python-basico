9dd6084ab04f1401785b9945673c30d5ebfc6f6a
brenoresende <167318207+brenoresende@users.noreply.github.com>
2024-04-17 19:43:52 -0300

    Add files via upload

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,17 @@
+'''
+Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+Armazene o valor 5 em uma variável e o valor 2 em outra variável
+Imprima o tipo de dados dessas duas variáveis
+Divida a primeira variável (com o valor 5) pela segunda variável (com o valor 2) e armazene o resultado em uma terceira variável
+Imprima a terceira variável e também o seu tipo
+'''
+
+
+x = 5
+y = 2
+
+print('Tipo da variável X: ', type(x))
+print('Tipo da variável Y: ', type(y))
+
+z = x/y
+print(f'O valor da variável é {z}, e o tipo dela é {type(z)}')
---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,16 @@
+'''
+Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+Some os valores da segunda e terceira variável e armazene em outra variável.
+Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+'''
+
+texto = 'o resultado é'
+n1 = 10
+n2 = 3.5
+soma = n1 + n2
+
+print(f'O valor da primeira variável é: "{texto}" e o tipo é {type(texto)}')
+print(f'O valor da segunda variável é: "{n1}" e o tipo é {type(n1)}')
+print(f'O valor da terceira variável é: "{n2}" e o tipo é {type(n2)}')
+print(f'O valor da quarta variável é: "{soma}" e o tipo é {type(soma)}')
