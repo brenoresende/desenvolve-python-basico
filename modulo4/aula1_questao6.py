ceb5c059ff52c5fefc72ead4bd90ea7426cd92e4
brenoresende <167318207+brenoresende@users.noreply.github.com>
2024-04-25 14:24:51 -0300

    Add files via upload

--- modulo4/aula1_questao6.py
+++ modulo4/aula1_questao6.py
@@ -12,14 +12,31 @@ e o percentual de cada uma em relação ao total de cobaias utilizadas
 """
 
 
+n = int(input())
+i=0
 
+soma_sapo = 0
+soma_rato = 0
+soma_coelho = 0
 
+while i<n:
+    qnt = int(input())
+    op = input().upper()
+    i += 1
+    if op == 'S':
+        soma_sapo += qnt
+    if op == 'R':
+        soma_rato += qnt
+    if op == 'C':
+        soma_coelho += qnt
 
+total = soma_sapo + soma_rato + soma_coelho
 
-
-
-
-
-
-
+print(f'Total: {total}')
+print(f'Total de coelhos: {soma_coelho}')
+print(f'Total de ratos: {soma_rato}')
+print(f'Total de sapos: {soma_sapo}')
+print(f'Percentual de coelhos: {soma_coelho/total*100:.2f} %')
+print(f'Percentual de ratos: {soma_rato/total*100:.2f} %')
+print(f'Percentual de sapos: {soma_sapo/total*100:.2f} %')
 
